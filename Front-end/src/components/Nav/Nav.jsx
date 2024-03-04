e2ee98a72aad34270829a5f91f77120d18945843
Ferris Chang <103004499+PandaFChangs@users.noreply.github.com>
2024-03-04 16:58:52 -0500

    fonts added

--- Front-end/src/components/Nav/Nav.jsx
+++ Front-end/src/components/Nav/Nav.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Nav.css'
 
 const Nav = () => {
+  const [scrollPosition, setScrollPosition] = useState(0)
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const position = window.scrollY
+      setScrollPosition(position)
+    }
+
+    window.addEventListener('scroll', handleScroll, { passive: true })
+    
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
+    }
+  }, [])
+
+
+
+
   return (
-    <div className='Nav-bar flex-container'>
+    <div className='Nav-bar flex-container' 
+    id={scrollPosition > 0 ? 'scrollStyle' : 'fixedStyle'}>
       <a href='#Home' className='Nav-btn flex-container'>Home</a>
       <a href='#Projects' className='Nav-btn flex-container'>Projects</a>
       <a href='#Contact' className='Nav-btn flex-container'>Socials</a>
