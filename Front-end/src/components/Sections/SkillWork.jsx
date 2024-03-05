cd72c849b6d8ca5b60252941a8a36e1b7c320ff3
Ferris Chang <103004499+PandaFChangs@users.noreply.github.com>
2024-03-05 14:30:21 -0500

    Jobs added to json file

--- Front-end/src/components/Sections/SkillWork.jsx
+++ Front-end/src/components/Sections/SkillWork.jsx
@@ -1,18 +1,58 @@
 import React, { useState } from 'react'
 import './SkillWork.css'
+import document from '../../assets/skills.json'
 
 const SkillWork = () => {
   const [skills, setSkills] = useState([])
+  const [work, setWork] = useState([])
+  const parsedSkills = document.skills
+  const parsedWork = document.work
+  
+  useState(() => {
+    try {
+      setSkills(parsedSkills)
+      setWork(parsedWork)
+    } catch (err) {
+      console.error('Error fetching document', err.message)
+    }
+  },[])
 
-  return (
+  return ( 
     <div id='SkillWork-Section' className='flex-container'>
-      <h2 id='special-skill' className='subheading-font'>Skills & Languages</h2>
-      <div>
-        {
-
-        }
+      <h2 id='special-title-format' className='subheading-font'>Skills</h2>
+      <div id='skill-container' className='flex-container'>
+        {skills.map((item, index) => (
+          <div id='skills' className='flex-container' key={index}>
+            {item}
+          </div>          
+        ))}
       </div>
-      <div></div>
+      <h2 id='special-work-format' className='subheading-font'>Experience</h2>
+      <div id='experience-container' className='flex-container'>
+        {work.map((item, index) => (
+          <div id='experience' className='flex-container' key={index}>
+            <div>{item.year}</div>
+            <div>
+              <div>
+                <div>
+                  {item.title}
+                </div>
+                <div>
+                  {item.company}
+                </div>
+              </div>
+              <div>
+                <div>{item.desc}</div>
+                <div>
+                  {item.skills.map((element, idx) => (
+                  <p key={idx}>{element}</p>
+                  ))}
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
